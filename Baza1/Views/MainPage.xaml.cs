29f31e4edd1ddb53285b97803e1fb0a6372d04c2
kt61365 <s61365@student.ubb.edu.pl>
2025-05-28 01:55:34 +0200

    V2

--- Baza1/Views/MainPage.xaml.cs
+++ Baza1/Views/MainPage.xaml.cs
@@ -6,6 +6,8 @@ namespace Baza1;
 public partial class MainPage : ContentPage
 {
     private readonly DatabaseService _databaseService;
+    private string _currentSortColumn;
+    private bool _isAscending = true;
 
     public MainPage(DatabaseService databaseService)
     {
@@ -91,5 +93,57 @@ public partial class MainPage : ContentPage
                 $"Typ umowy: {pracownik.Typ_umowy}",
                 "OK");
         }
+    }
+
+    private async void OnHeaderClicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        var sortColumn = button?.CommandParameter?.ToString();
+
+        if (string.IsNullOrEmpty(sortColumn))
+            return;
+
+        if (_currentSortColumn == sortColumn)
+        {
+            _isAscending = !_isAscending;
+        }
+        else
+        {
+            _currentSortColumn = sortColumn;
+            _isAscending = true;
+        }
+
+        var pracownicy = await _databaseService.GetPracownicyAsync();
+        var sortedPracownicy = pracownicy.AsEnumerable();
+        switch (sortColumn)
+        {
+            case "Id_pracownika":
+                sortedPracownicy = _isAscending
+                    ? sortedPracownicy.OrderBy(p => p.Id_pracownika)
+                    : sortedPracownicy.OrderByDescending(p => p.Id_pracownika);
+                break;
+            case "Imie":
+                sortedPracownicy = _isAscending
+                    ? sortedPracownicy.OrderBy(p => p.Imie)
+                    : sortedPracownicy.OrderByDescending(p => p.Imie);
+                break;
+            case "Nazwisko":
+                sortedPracownicy = _isAscending
+                    ? sortedPracownicy.OrderBy(p => p.Nazwisko)
+                    : sortedPracownicy.OrderByDescending(p => p.Nazwisko);
+                break;
+            case "Typ_umowy":
+                sortedPracownicy = _isAscending
+                    ? sortedPracownicy.OrderBy(p => p.Typ_umowy)
+                    : sortedPracownicy.OrderByDescending(p => p.Typ_umowy);
+                break;
+            case "Pesel":
+                sortedPracownicy = _isAscending
+                    ? sortedPracownicy.OrderBy(p => p.Pesel)
+                    : sortedPracownicy.OrderByDescending(p => p.Pesel);
+                break;
+        }
+
+        pracownicyCollection.ItemsSource = sortedPracownicy.ToList();
     }
 }
